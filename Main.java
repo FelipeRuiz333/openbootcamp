fdb6da214953176e545e4b278f60fe27743847b2
FelipeRuiz333 <123210691+FelipeRuiz333@users.noreply.github.com>
2023-01-25 20:18:43 -0300

    estructuras de control

--- Main.java
+++ Main.java
@@ -1,24 +1,54 @@
 public class Main {
     public static void main(String[] args) {
-        suma(10,20,30);
 
-        Coche miCoche = new Coche();
-        miCoche.ContarPuertas();
-        System.out.println(miCoche.puertas);
+        // IF
 
-    }
-    public static void suma(int a, int b, int c){
-        int resultado;
-        resultado = a + b +c;
+        var numeroIf = 3;
 
-        System.out.println(resultado);
-    }
-}
+        if (numeroIf == 0) {
+            System.out.println("El número es 0.");
+        } else if (numeroIf < 0) {
+            System.out.println("El número es Negativo.");
+        } else {
+            System.out.println("El número es Positivo.");
+        }
 
-class Coche {
-    public int puertas = 0;
+        // WHILE
 
-    public void ContarPuertas(){
-        this.puertas++;
+        var numeroWhile = 1;
+
+        do {
+            System.out.println(numeroWhile);
+            numeroWhile = numeroWhile + 1;
+        } while (numeroWhile < 3);
+            numeroWhile = numeroWhile + 1; //numeroWhile++
+            System.out.println(numeroWhile);
+
+        // FOR
+
+
+        for (int numeroFor = 0; numeroFor <= 3; numeroFor++){
+            System.out.println(numeroFor);
+        }
+
+        // SWITCH
+
+        var estacion = "verano";
+        switch (estacion){
+            case "verano":
+                System.out.println("Es verano");
+                break;
+            case "primavera":
+                System.out.println("Es primavera");
+                break;
+            case "otoño":
+                System.out.println("Otoño");
+                break;
+            case "invierno":
+                System.out.println("Es invierno");
+                break;
+            default:
+                System.out.println(estacion);
+        }
     }
 }
