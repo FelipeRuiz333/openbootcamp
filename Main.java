25b093d5c6d3a6de393844c99de14190c9d3a609
FelipeRuiz333 <123210691+FelipeRuiz333@users.noreply.github.com>
2023-02-02 03:34:46 -0300

    Ejercicio tema 9

--- Main.java
+++ Main.java
@@ -1,43 +1,91 @@
 public class Main {
     public static void main(String[] args) {
 
-        Persona persona = new Persona();
+        Cliente cliente = new Cliente();
+        cliente.edad = 25;
+        cliente.nombre = "Felipe";
+        cliente.telefono = 40167938;
+        cliente.credito = 150.00;
 
-        persona.setEdad(25);
-        persona.setNombre("ramon");
-        persona.setTelefono(4219682);
-        System.out.println("La edad es: "+persona.getEdad());
-        System.out.println("El nombre es: "+persona.getNombre());
-        System.out.println("El telefono es: "+persona.getTelefono());
+        System.out.println("ESTA ES LA CLASE CLIENTE");
+        System.out.println("La edad es: " + cliente.getEdad());
+        System.out.println("El nombre es: " + cliente.getNombre());
+        System.out.println("El telefono es: " + cliente.getTelefono());
+        System.out.println("Credito: " + cliente.getCredito());
+
+        Trabajador trabajador = new Trabajador();
+        trabajador.edad = 30;
+        trabajador.nombre = "Ramon";
+        trabajador.telefono = 1312312;
+        trabajador.salario = 2500.00;
+
+        System.out.println("ESTA ES LA CLASE TRABAJADOR");
+        System.out.println("La edad es: " + trabajador.getEdad());
+        System.out.println("El nombre es: " + trabajador.getNombre());
+        System.out.println("El telefono es: " + trabajador.getTelefono());
+        System.out.println("Credito: " + trabajador.getSalario());
     }
 }
 class Persona {
-    private int edad;
-    private String nombre;
-    private int telefono;
+    int edad;
+    String nombre;
+    int telefono;
 
-    //edad
-    public void setEdad(int edad){
-        this.edad = edad;
-    }
-    public int getEdad(){
+    //EDAD
+    public int getEdad() {
         return edad;
     }
 
-    //nombre
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setEdad(int edad) {
+        this.edad = edad;
     }
+
+    //NOMBRE
     public String getNombre() {
         return nombre;
     }
 
-    //telefono
-    public void setTelefono(int telefono) {
-        this.telefono = telefono;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
+
+    //TELEFONO
     public int getTelefono() {
         return telefono;
     }
 
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+}
+
+    //CLIENTE
+class Cliente extends Persona {
+    double credito;
+
+    public double getCredito() {
+        return credito;
+    }
+
+    public void setCredito(double credito) {
+        this.credito = credito;
+    }
+}
+
+    //INFOCLIENTE
+class InfoCliente extends Cliente{
+
+}
+
+//TRABAJADOR
+class Trabajador extends Persona {
+    double salario;
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 }
